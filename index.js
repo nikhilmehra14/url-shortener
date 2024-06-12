50dd870e0c1c976d86c06d0a7e64336c49787c6a
Nikhil Mehra <nikhilmehra248@gmail.com>
2024-06-13 01:51:34 +0530

    auth done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedInUserOnly, checkAuth } = require("./middlewares/auth");
 const { connectToMongoDB } = require("./connect");
 const app = express();
+
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +21,10 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedInUserOnly, urlRoute);
+app.use("/", checkAuth, staticRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => console.log(`Server is started at PORT: ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { userSignUp, userLogIn } = require("../controllers/user");
+const router = express.Router();
+
+router.post("/", userSignUp);
+router.post("/login", userLogIn);
+module.exports = router;
